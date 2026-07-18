8715c49d1a1c49a08f3cb692662a56d195c6df11
Jmrqz <d0dengph@gmail.com>
2026-07-18 12:10:50 +0800

    Add files via upload
    This is the source code!

---
+++ visual_email/emailvisual.py
@@ -0,0 +1,13 @@
+
+#SIMPLE EMAIL CREATOR WITH AUTOMATIC CLEANUP
+
+#Input user birth name and email, removing whitespaces and numbers in the registered name
+fn = input("What is your First Name? ").title().lstrip().strip("1" "2" "3" "4" "5" "6" "7" "8" "9" "0").rstrip()
+ln = input("What is your Last Name? ").title().lstrip().strip("1" "2" "3" "4" "5" "6" "7" "8" "9" "0").rstrip()
+email = input("What is your Email? ").lower().strip()
+
+#if user inputs an invalid email (eg: typo in email or unsupported email)
+if "@gmail.com" not in email and "@yahoo.com" not in email and "@outlook.com": #supported email domains
+    print("Invalid email, please try again.")
+else: 
+    print(f"Account created for {fn} {ln} ({email})") #output when things go as intended
